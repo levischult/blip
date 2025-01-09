990826b6bae4c10351b63c2aa962331bac339ea6
levischult <lss8ak@virginia.edu>
2025-01-09 11:48:40 -0600

    early progress on lisa_orbits_keplerian. figuring out eccentric anomaly translation

--- blip/src/geometry.py
+++ blip/src/geometry.py
@@ -82,6 +82,172 @@ class geometry(sph_geometry):
         rs3 = np.array([x_n[:, 2],y_n[:, 2],z_n[:, 2]])
 
         return rs1, rs2, rs3
+    
+    def lisa_orbits_keplerian(self, tsegmid, L=None, a=1.496e11, lambda1=0, m_init1=0, kepler_order=2):
+
+
+        '''
+        Levi Schult 2025 01 08
+
+        Define LISA orbital positions at the midpoint of each time integration segment 
+        using Keplerian orbits - arm lengths will vary. 
+        
+        This is heavily-based and draws greatly from the KeplerianOrbits class 
+        in the LisaOrbits package: https://lisa-simulation.pages.in2p3.fr/orbits/html/latest/keplerian.html
+        Orbits are the solutions to two-body problem in Newtonian gravity 
+        (Earth gravity is neglected). Arm flexing is minimized in next-to 
+        leading order in eccentricity. The math is written well at the page linked
+        above.
+
+        Parameters
+        -----------
+
+        tsegmid  :  array
+            A numpy array of the tsegmid for each time integration segment.
+        
+        L  :  float
+            mean inter-spacecraft distance [m]. Default uses self.armlength.
+
+        a  :  float
+            semi-major axis for an orbital period of 1 yr [m]. Default is 1 AU=
+            1.496e11 m
+        
+        lambda1  :  float
+            spacecraft 1's longitude of periastron [rad] default=0
+
+        m_init1  :  float
+            spacecraft 1's mean anomaly at initial time [rad] default=0
+
+        kepler_order  :  int
+            number of iterations in the Newton-Raphson procedure. default=2
+
+        Returns
+        -----------
+        rs1, rs2, rs3  :  array
+            Arrays of satellite positions for each segment midpoint in timearray. e.g. rs1[1] is [x1,y1,z1] at t=midpoint[1]=timearray[1]+(segment length)/2.
+        '''
+        # LSS 20250108 - setting armlength to self.armlength as default
+        if L is None:
+            L = self.armlength
+
+        ## Branch orbiting and stationary cases; compute satellite position in stationary case based off of first time entry in data.
+        if self.params['lisa_config'] == 'stationary':
+            # Calculate start time from tsegmid
+            tstart = tsegmid[0] - (tsegmid[1] - tsegmid[0])/2
+            # Fill times array with just the start time
+            times = np.empty(len(tsegmid))
+            times.fill(tstart)
+        elif self.params['lisa_config'] == 'orbiting':
+            times = tsegmid
+        else:
+            raise ValueError('Unknown LISA configuration selected')
+        
+        # LSS 20250108 - perturbation to tilt angle nu. This minimizes breating
+        # of LISA constellation. For details, see arXiv:gr-qc/0507105
+        delta = 5.0 / 8.0
+
+        # LSS 20250108 - orbital parameter used for series expansions
+        alpha = L / (2 * a)
+
+        # LSS 20250108 - LISA constellation's  orbital tilt angle to the ecliptic
+        nu = (np.pi / 3.0) + (delta * alpha)
+
+        # LSS 20250108 - Orbital eccentricity
+        e = np.sqrt(1 + ((4 * alpha * np.cos(nu)) / np.sqrt(3)) + ((4 * alpha**2) / 3)) - 1
+
+        # LSS 20250108 - now we calculate things that are necessary later for
+        # position calculations
+        tan_i = alpha * np.sin(nu) / ((np.sqrt(3) / 2) + alpha * np.cos(nu))
+        cos_i = 1 / np.sqrt(1 + tan_i**2)
+        sin_i = tan_i * cos_i
+    
+
+        ## Alpha and beta phases allow for changing of initial satellite orbital phases; default initial conditions are alphaphase=betaphase=0.
+        betaphase = 0
+        alphaphase = 0
+
+        ## Orbital angle alpha(t)
+        at = (2*np.pi/31557600)*times + alphaphase
+
+        ## Eccentricity. L-dependent, so needs to be altered for time-varied arm length case.
+        e = self.armlength/(2*a*np.sqrt(3))
+
+        # LSS 20250109 - beta_n is self.theta in lisa orbits.
+        # LSS 20250109 - I think that m_init1 in lisa orbits is betaphase in blip
+        # LSS 20250109 - and lambda1 is alphaphase in blip.
+        # LSS 20250109 - so m_init = beta_n and at = 
+        ## Initialize arrays
+        beta_n = (2/3)*np.pi*np.array([0,1,2])+betaphase
+
+        '''
+        Estimate the eccentric anomaly.
+
+        This is heavily-based and draws greatly from the KeplerianOrbits class 
+        in the LisaOrbits package: https://lisa-simulation.pages.in2p3.fr/orbits/html/latest/keplerian.html
+        
+        Their docstring explains the math:
+        This uses an iterative Newton-Raphson method to solve the Kepler equation,
+        starting from a low eccentricity expansion of the solution.
+
+        .. math::
+            \psi_k - e \sin \psi_k = m_k(t) \qc
+
+        with :math:`m_k(t)` the mean anomaly.
+
+        We use ``kepler_order`` iterations. For low eccentricity, the convergence rate
+        of this iterative scheme is of the order of :math:`e^2`. Typically for LISA
+        spacecraft (characterized by a small eccentricity 0.005), the iterative
+        procedure converges in one iteration using double precision.
+        '''
+
+
+        
+        
+        
+        
+        
+        
+        
+        ## meshgrid arrays
+        Beta_n, Alpha_t = np.meshgrid(beta_n, at)
+
+
+
+
+        ## Calculate inclination and positions for each satellite.
+        x_n = a*np.cos(Alpha_t) + a*e*(np.sin(Alpha_t)*np.cos(Alpha_t)*np.sin(Beta_n) - (1+(np.sin(Alpha_t))**2)*np.cos(Beta_n))
+        y_n = a*np.sin(Alpha_t) + a*e*(np.sin(Alpha_t)*np.cos(Alpha_t)*np.cos(Beta_n) - (1+(np.cos(Alpha_t))**2)*np.sin(Beta_n))
+        z_n = -np.sqrt(3)*a*e*np.cos(Alpha_t - Beta_n)
+
+
+        ## Construct position vectors r_n
+        rs1 = np.array([x_n[:, 0],y_n[:, 0],z_n[:, 0]])
+        rs2 = np.array([x_n[:, 1],y_n[:, 1],z_n[:, 1]])
+        rs3 = np.array([x_n[:, 2],y_n[:, 2],z_n[:, 2]])
+
+        return rs1, rs2, rs3
+
+    def compute_eccentric_anomaly(t, sc):
+        '''
+        Estimate the eccentric anomaly.
+
+        This is heavily-based and draws greatly from the KeplerianOrbits class 
+        in the LisaOrbits package: https://lisa-simulation.pages.in2p3.fr/orbits/html/latest/keplerian.html
+        
+        Their docstring explains the math:
+        This uses an iterative Newton-Raphson method to solve the Kepler equation,
+        starting from a low eccentricity expansion of the solution.
+
+        .. math::
+            \psi_k - e \sin \psi_k = m_k(t) \qc
+
+        with :math:`m_k(t)` the mean anomaly.
+
+        We use ``kepler_order`` iterations. For low eccentricity, the convergence rate
+        of this iterative scheme is of the order of :math:`e^2`. Typically for LISA
+        spacecraft (characterized by a small eccentricity 0.005), the iterative
+        procedure converges in one iteration using double precision.
+        '''
 
 
     def doppler_response(self, f0, theta, phi, tsegmid, tsegstart):
